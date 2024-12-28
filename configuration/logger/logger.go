b287e50d8b2477dc4a49778547d99f70be8c23f6
Vitor Rodrigues <vitor.rodrigues@gmail.com>
2024-12-28 10:33:03 -0300

    setting outputs

--- configuration/logger/logger.go
+++ configuration/logger/logger.go
@@ -21,6 +21,8 @@ func init() {
 			EncodeTime:   zapcore.ISO8601TimeEncoder,
 			EncodeCaller: zapcore.ShortCallerEncoder,
 		},
+		OutputPaths:      []string{"stdout"},
+		ErrorOutputPaths: []string{"stderr"},
 	}
 
 	log, _ = logConfiguration.Build()
